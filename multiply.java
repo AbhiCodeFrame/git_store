2591c44d837a0febd6d26e69af5e0df05cd30da1
Abhishek <abhishek0199920@gmail.com>
2021-10-26 11:03:47 +0530

    added multiply.java

---
+++ multiply.java
@@ -0,0 +1,16 @@
+class Multiply
+{
+    public static void main(String[] args)
+    {
+        int num1=5;
+        int num2=10;
+
+        int c = a*b;
+
+        System.out.println(c);
+
+       
+        
+    }
+
+}g
